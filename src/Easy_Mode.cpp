ed7a9714c4712a7d7f8651a8ae810d83d2d3a0aa
AsmirHabibi <gamerasmir134@gmail.com>
2023-12-16 20:55:53 +0500

    Update Easy_Mode.cpp

--- src/Easy_Mode.cpp
+++ src/Easy_Mode.cpp
@@ -5,81 +5,79 @@
 #include <cstdlib>
 #include <ctime>
 #include "../header/Easy_Mode.hpp"
+#include "../header/Medium_Mode.hpp"
 
 using namespace std;
+
+
 vector<string> options = {"Hello", "Banana", "Cherry", "Mango", "Pineapple",
                           "Strawberry", "Watermelon", "Grape", "Kiwi", "Orange", "Blueberry", "Carrot", "Broccoli", "Spinach", "Tomato", "Cucumber", "Bell Pepper", "Eggplant",
                           "Lettuce", "Potato", "Onion", "Cabbage", "Poor", "Happy"};
-
 void Easy_Mode::mainSystem()
 {
     srand(static_cast<unsigned int>(time(0)));
-
+    
     int vectorSize = options.size();
+    
     int randomIndex = rand() % vectorSize;
+    
     const string &selectedString = options[randomIndex];
+    
+    string displayString(selectedString.length(), '-');
 
-    string displayStringM(selectedString.length(), '-');
-    attempts = 10;
-
-    cout << "Welcome To Easy Mode" << endl;
+    attempts = 20;
+    cout << "Welcome To Eassy Mode" << endl;
     sleep(1);
-    cout << "You will have to guess a word, and you have 20 attempts." << endl;
-    sleep(1);
+    cout << "You will have to guess a word, and you have 20 chances." << endl;
+    sleep(2);
     cout << "Good Luck!" << endl;
     sleep(1);
     system("cls");
-
     while (!wordGuessed && attempts > 0)
     {
-        displayStringM[0] = selectedString[0];
-        cout << "Current display: " << displayStringM << " " << attempts << " attempts left." << endl;
-
+        displayString[0] = selectedString[0];
+        cout << "Current display: " << displayString << " " << attempts << " attempts left." << endl;
         cout << "Guess a letter: ";
         cin >> userGuess;
         cout << endl;
 
-        correctGuess = false; // Initialize the flag before checking each letter
+        correctGuess = false; 
 
         for (int i = 0; i < selectedString.length(); ++i)
         {
             if (userGuess == selectedString[i])
             {
-                displayStringM[i] = userGuess;
+                displayString[i] = userGuess;
                 correctGuess = true;
             }
         }
-
-        if (correctGuess)
+        if (!correctGuess)
         {
             attempts--;
             cout << "Incorrect guess! Try again. (" << attempts << ") attempts left." << endl;
         }
 
-        if (displayStringM == selectedString)
+        if (displayString == selectedString)
         {
             wordGuessed = true;
             cout << "Congratulations! You guessed the word: " << selectedString << endl;
         }
     }
-
     if (!wordGuessed)
     {
         cout << "Ah Shucks! You ran out of attempts. The correct answer was " << selectedString << ".\n"
              << "Better Luck Next Time." << endl;
     }
-
     while (true)
     {
-        cout << "Enter 'r' to retry, 'h' to go to the next level, or 'e' to exit: ";
+        cout << "Enter 'r' to retry, 'm' to go to the next level, or 'e' to exit: ";
         cin >> ynretry;
-
         if (ynretry == 'r' || ynretry == 'R')
         {
             cout << "Retrying..." << endl;
             sleep(2);
             system("cls");
-            Easy_Mode easy_mode;
+            Easy_Mode eeee;
             break;
         }
         else if (ynretry == 'e' || ynretry == 'E')
@@ -88,13 +86,11 @@ void Easy_Mode::mainSystem()
             sleep(1);
             exit(0);
         }
-        else if (ynretry == 'h' || ynretry == 'H')
+        else if (ynretry == 'm' || ynretry == 'M')
         {
-            cout << "Going to Hard Mode" << endl;
-            sleep(1);
-            cout << "WIP!" << endl;
+            cout << "Going to Medium Level..." << endl;
             sleep(2);
-            exit(0);
+            Medium_Mode muahahaha;
         }
         else
         {
@@ -102,7 +98,6 @@ void Easy_Mode::mainSystem()
         }
     }
 }
-
 Easy_Mode::Easy_Mode()
 {
     mainSystem();
